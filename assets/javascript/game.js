35ace724e1ae82f004e1f33a1da64c63b7d9aa99
Andy Roush <andyroush17@gmail.com>
2018-02-22 18:26:33 -0800

    fully functional now. Still working on the finer details

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,128 +1,138 @@
 // global variables
-var wordBank = ['fresh Prince', 'friends', 'seinfeld', 'full house', 'family matters', 'saved by the bell',
-'home improvement', 'dinosaurs', 'martin', 'rockos modern life', 'boy meets world', 'doug', 'roseanne', 'in living color',
+var wordList = ['freshprince', 'friends', 'seinfeld', 'fullhouse', 'familymatters', 'savedbythebell',
+'homeimprovement', 'dinosaurs', 'martin', 'rockosmodernlife', 'boymeetsworld', 'doug', 'roseanne', 'inlivingcolor',
 'animaniacs', ];
-// win-counter
-var wins = 0; 
-// loss-counter
-var loss = 0;
-// wrong-guess
-var wrongGuesses = [];
-// guesses-left
-var guessesLeft;
-// Underscores in current word
-var underScores = [];
-// used to pull from this to the underscores
-var userGuesses = [];
-// from wordBank
-var word = ''
-var answerArray = [];
-//
-var rightGuess = 0
 
+   
+   var chosenWord = "";
+   var letterInChosenWord = [];
+   var numBlanks = 0;
+   var blanksAndSuccesses = [];
+   var wrongGuesses = [];
+   
+   var winCounter = 0;
+   var lossCounter = 1;
+   var numGuesses = 15;
+   
+   function startGame(){
+   /*
+   1. computer chooses a word from word list
+   2. computer breaks down that random word as letters and replace them with
+   underscores _
+   3. add those underscores to the HTML to display to the player
+   4. numguesses always equals 15, and blankandsuccess is an empty array, 
+   and wronggueses is empty as well.
+   */
+   wrongGuesses = [];
+   console.log("this is wrong guesses in startGame", wrongGuesses);
+   numGuesses = 15;
+   blanksAndSuccesses = [];
+   
+   
+   chosenWord = wordList[Math.floor(Math.random() * wordList.length)];
+   lettersInChosenWord = chosenWord.split("");
+   numBlanks = lettersInChosenWord.length;
+   console.log(chosenWord);
+   console.log(numBlanks)
+   
+   for(var i = 0; i < numBlanks; i++){
+       blanksAndSuccesses.push("_");
+   }
+   console.log(blanksAndSuccesses);
+   document.getElementById('word-blank').innerHTML = blanksAndSuccesses.join(" ");
+   document.getElementById('guesses-left').innerHTML = numGuesses;
 
-// startgame function
-function startGame() {
-
-var word = wordBank[Math.floor(Math.random() * wordBank.length)];
-for ( var i = 0; i < word.length; i++) {
-    console.log(word)
-    if (word[i] === " ") {
-        answerArray[i] = " "
-    }
-    else {
-        answerArray[i] = "_"
-    }
-}
-}
-
-/* var underScores = document.getElementById("underScore");
-// underScores.innerHTML = answerArray;
-underScores.innerHTML = answerArray.join(" ");
-
-    // reset
-    wrongGuesses = [];
-    guessesLeft = 10;
-
-    // HTML
-    document.getElementById('guesses-left').textContent = guessesLeft;
-    document.getElementById('wrong-guess').textContent = wrongGuesses;
-    document.getElementById('win-counter').textContent = wins;
-    document.getElementById('loss-counter').textContent = loss;
-
-}
-
-    // User's guesses
-document.onkeyup = function(event) {
-    userGuesses = event.key;
-    console.log(userGuesses)
-    console.log(answerArray)
-        if(word.indexOf(userGuesses) > -1) {
-            for( var i = 0; i < word.length; i++) {
-                if(word[i] === userGuesses) {
-                    answerArray[i] = userGuesses;
-                    document.getElementById("underScore").innerHTML = answerArray.join(' ')
-                }
-
-            }
-        }
-     
-            
-        
-        else {
-            wrongGuesses.push(userGuesses);
-            guessesLeft--;
-        }
-    }
- */
-
-    
-    
-    
-    
-    
- /*    startGame() */
-//     if (word.indexOf(userGuesses) > -1) {
-//         for ( var i = 0; i < word.length; i++) {
-//             if ( randWord[i] === userGuesses) {
-//                 underScores[i] == userGuesses;
-//                 wins++;
-//                 winLose();
-//                 console.log(underScores)
-//             }
-//         }
-//     }
-//     else {
-//         wrongLetter.push(userGuesses);
-//         guessesLeft--;
-//         console.log(wrongLetter)
-//         // winLose();
-//     }
-// }
-
-
-// Main
-
-
-// onkey up = X 
-// for (the length of the random word)
-// if x = random word [i]  {
-//     add it to the underscores Array.
-// } 
-// else {
-//     add it to the wrong guesses Array. 
-//     guessesleft-- (decrement)
-// }
-
-// Loop that until the word is complete 
-// when underscore.length === random word.length {
-//     player wins
-//     wins++
-//     change the seinfeld picute to the winning show's picture
-// }
-
-// if guesses-left === 0 {
-//     game Over
-//     losses ++
-
-
+   
+   
+   
+   }
+   
+   
+   function checkLetters(letter){
+       /*
+       1. Compare the letter the user picks matches any of the letters in the word
+       2. Determine if the letter the user picked
+       is in the word. If so, do something, if not, do something else
+       3. If the user is wrong, decrease the numGuesses variables by one
+       */
+   
+       var letterInWord = false;
+   
+       for(var i = 0; i < numBlanks; i++){
+           if(chosenWord[i] === letter){
+               letterInWord = true;
+   
+           }
+       }
+   
+       if(letterInWord){
+           for(i = 0; i < numBlanks; i++){
+               if(chosenWord[i] === letter){
+               blanksAndSuccesses[i] = letter;
+   
+           }
+   
+           }
+       }else{
+           numGuesses --;
+           wrongGuesses.push(letter)
+       }
+   
+       /*
+       to check if a letter is already in the wrong guesses array. What we want to do
+       is set up an if/else conditional that will run a for loop that will iterate over
+       all the letters and then use the if/else to check if it it already exists.
+       */
+   
+   
+   }
+   
+   
+   function roundComplete(){
+       /*
+       1. Will update the HTML with letters that are in the word
+       2. Is going to update the HTML with guesses we have left
+       3. Is going to update the HTML to show the wrong guesses
+       4. Is going to determine whether the use won the game or not
+       */
+   
+       document.getElementById('word-blank').innerHTML = blanksAndSuccesses.join(" ");
+       document.getElementById('guesses-left').innerHTML = numGuesses;
+       document.getElementById('wrong-guesses').innerHTML = wrongGuesses.join(" ");
+   
+   
+       // if(blanksAndSuccesses.indexOf(letter >= 1)){
+       //     console.log(letter)
+       // }
+       console.log(lettersInChosenWord);
+       console.log(blanksAndSuccesses);
+       if(lettersInChosenWord.join(" ") === blanksAndSuccesses.join(" ")){
+           winCounter++;
+        //    alert("Congrats you 90s master!");
+           document.getElementById("winimg").src = "../images/cup.jpg"; //Not working right now.
+           document.getElementById('win-counter').innerHTML = winCounter;
+           startGame();
+       }else if(numGuesses === 0){
+           document.getElementById('loss-counter').innerHTML  = lossCounter ++;
+           document.getElementById('wrong-guesses').innerHTML = "";
+           alert("you don't have any more guesses");        
+           startGame();
+       }
+   
+   
+   
+   
+   }
+   startGame();
+   document.onkeyup = function(event){
+       /*
+       1. its going to take in the letter that we type in
+       2. its going to pass it through the CheckLetter function 
+       */
+       var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+       console.log("this is the letter we typed", letterGuessed)
+       checkLetters(letterGuessed)
+       roundComplete();
+   
+   
+   }
